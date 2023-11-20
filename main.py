675bc4b98c26f22b9dc28cea2b37a71f91df9056
Vladislav Tritner <tritner111@gmail.com>
2023-11-20 18:02:18 +0200

    changed the code so it will use json files

--- main.py
+++ main.py
@@ -3,22 +3,49 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
-
+import json
 
 
 def save():
-    website = Website_entry.get()
+    Website = Website_entry.get()
     username = Username_entry.get()
     password = Password_entry.get()
-    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+    new_data = {
+        Website :{
+            "username" : username,
+            "password" : password
+        }
+    }
+    if len(Website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showerror(title="Something wrong",message="do not leave empty fields")
     else:
-        answer = messagebox.askokcancel(title=website,message=f"these are the details you entred\nEmail: {username}\n Password: {password}\nDO YOU CONFIRM? ")
-        if answer == 1:
-            with open("data.txt","a") as data_file:
-                data_file.write(f"{website} | {username} | {password}\n")
-                Website_entry.delete(0,END)
-                Password_entry.delete(0,END)
+        try:
+            with open("data.json","r") as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+            with open("data.json","w") as data_file:
+                json.dump(data,data_file,indent=4)
+        except FileNotFoundError:
+            with open("data.json","w") as data_file:
+                json.dump(new_data,data_file,indent=4)
+
+        finally:
+            Website_entry.delete(0, END)
+            Password_entry.delete(0, END)
+
+def search():
+    website = Website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Something wrong", message="dont have file with passwords yet!")
+    if website in data:
+        messagebox.showinfo(title="your username and password", message=f"Username: {data[website]["username"]}\nPassword: {data[website]["password"]}")
+    else:
+        messagebox.showerror(title="Something wrong", message="you dont have password for this website")
+
+
 def generate():
     Password_entry.delete(0, END)
     #generate random strong password
@@ -56,9 +83,9 @@ canvas.create_image(100,100,image=Lock_img)
 
 Website_label = Label(text="Website:")
 Website_label.grid(column=0,row=1)
-Website_entry = Entry(width=52)
+Website_entry = Entry(width=34)
 Website_entry.focus()
-Website_entry.grid(column=1,row=1,columnspan=2)
+Website_entry.grid(column=1,row=1,columnspan=2,sticky="w")
 
 Username_label = Label(text="Email/Username:")
 Username_label.grid(column=0,row=2)
@@ -68,14 +95,17 @@ Username_entry.grid(column=1,row=2,columnspan=2)
 
 Password_label = Label(text="Password:")
 Password_label.grid(column=0,row=3)
-Password_entry = Entry(width=52)
-Password_entry.grid(column=1,row=3,columnspan=2)
+Password_entry = Entry(width=33)
+Password_entry.grid(column=1,row=3,columnspan=2,sticky="w")
 
 add_button = Button(text="Add",width=44,command=save)
 add_button.grid(column=1,row=4,columnspan=2)
 
-generate_button = Button(text="Generate password",command=generate)
+generate_button = Button(text="Generate  password",command=generate)
 generate_button.grid(column=2,row=3,columnspan=2,sticky="w")
+
+search_button = Button(text="Search    password ",command=search)
+search_button.grid(column=2,row=1,columnspan=1)
 
 window.mainloop()
 
